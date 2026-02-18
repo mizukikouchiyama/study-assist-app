b9be0f91b84f49b090551510a054b07d2535ccc1
Mizuki Kouchiyama <mizukikouchiyama@MizukinoMacBook-Pro.local>
2026-02-18 15:15:52 +0900

    Add PWA support with brain icon

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'study-assist-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  // Vite assets will be handled by the build process usually, 
+  // but for a simple SW we can cache core files.
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
